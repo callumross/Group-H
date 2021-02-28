85105abeeaf9aefe1ccbd528be257f7c273a49c7
Callum Ross <callumr.ross97@gmail.com>
2021-02-28 14:47:48 +0000

    Changed sign up form and connected database

--- signUpUsers.php
+++ signUpUsers.php
@@ -1,24 +1,31 @@
 <?php
+// Include file to connect to database
 include("connection.php");
+// If a post request has not been submitted then redirect
 if(!isSet($_POST)){
     echo "Error";
-    header("Location: index.php");
+    header("Location: Index.html");
     die();
 } else {
+    // Save all the POST values into variables
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
     $username = $_POST["username"];
     $password = $_POST["password"];
-    $firstname = $_POST["firstname"];
-    $lastname = $_POST["lastname"];
-    $phonenumber = $_POST["Phonenumber"];
+    $postCode = $_POST["postCode"];
+    $contactNumber = $_POST["contactNumber"];
     
-    $sql_query = "INSERT INTO users (firstName,lastName,username,phoneNumber,password) VALUES ('$firstname','$lastname','$username','$phoneNumber','$password')";
+    // Run sql query to add information to database
+    $sql_query = "INSERT INTO users (firstName,lastName,username,password,postCode,contactNumber) VALUES ('$firstName','$lastName','$username','$password','$postCode','$contactNumber')";
 
+    // If the query is succesfull nothing happens, else echo an error message
     if(mysqli_query($db,$sql_query)){
     } else {
         echo "Error";
     }
+    // Save the username in a session variable so the system knows the user is logged in
     session_start();
     $_SESSION["username"] = $username;
-    header("Location: index.php");
+    header("Location: Index.html");
 }
 ?>
